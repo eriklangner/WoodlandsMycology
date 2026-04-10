ff6d7b23e0a7665ece0581e7f771d8f601f26548
Erik Langner <67337919+eriklangner@users.noreply.github.com>
2026-04-10 15:02:11 -0500

    Home view: use MushroomDetail for gallery, order by date desc

--- woodlandsmushrooms/views.py
+++ woodlandsmushrooms/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from .models import Mushroom
+from django.http import HttpResponse
+from blog.models import MushroomDetail
 
 
 # Create your views here.
 
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the woodlandsmushrooms index.")
+        return HttpResponse("Hello, world. You're at the woodlandsmushrooms index.")
 
 def home(request):
-    mushrooms = Mushroom.objects.all()
-    return render(request, 'woodlandsmushrooms/home.html', {'mushrooms':mushrooms})
-
+        mushrooms = MushroomDetail.objects.all().order_by('-date')
+        return render(request, 'woodlandsmushrooms/home.html', {'mushrooms': mushrooms})
